b7c08314edf38a5ca6090288f2657d2d68969450
Kyle Barlow <kb@kylebarlow.com>
2022-01-02 14:41:02 -0800

    Script done

--- fix_missing_thumbnails.py
+++ fix_missing_thumbnails.py
@@ -6,6 +6,7 @@ import io
 import time
 import hashlib
 import multiprocessing
+import shutil
 import tempfile
 from pprint import pprint
 
@@ -38,7 +39,7 @@ from tqdm import tqdm
 with DatabaseConnector() as conn:
     media = pd.read_sql_query(
         '''
-        SELECT media.s3_key, thumbnail.key as thumbnail_key, media.id AS media_id, thumbnail.id AS thumbnail_id
+        SELECT media.s3_key AS media_key, thumbnail.key as thumbnail_key, media.id AS media_id, thumbnail.id AS thumbnail_id, thumbnail.height as thumbnail_height, thumbnail.width as thumbnail_width
         FROM media
 JOIN thumbnail ON thumbnail.media_id=media.id
 WHERE media.media_type=0
@@ -65,7 +66,7 @@ all_s3_keys = []
 if os.path.isfile(all_s3_keys_cache):
     with open(all_s3_keys_cache, 'r') as f:
         for line in f:
-            all_s3_keys_cache.append(line)
+            all_s3_keys.append(line.strip())
 else:
     kwargs = {'Bucket': config.s3_bucket_name}
     while True:
@@ -84,71 +85,70 @@ else:
 print('%d total keys in S3' % len(all_s3_keys))
 
 s3_keys_df = pd.Series(all_s3_keys, name='thumbnail_key').to_frame()
+media_keys_df = s3_keys_df.copy().rename(columns={'thumbnail_key': 'media_key'})
 s3_keys_df = s3_keys_df.reset_index().rename(columns={'index': 's3_key_index'})
+media_keys_df = media_keys_df.reset_index().rename(columns={'index': 'media_key_index'})
 media = media.merge(s3_keys_df, on='thumbnail_key', how='left')
+media = media.merge(media_keys_df, on='media_key', how='left')
 
 print('%d thumbnails missing' % len(media.loc[media['s3_key_index'].isna()]))
+print('%d thumbnails missing also have missing media keys' % len(media.loc[media['s3_key_index'].isna() & media['media_key_index'].isna()]))
+try:
+    assert(len(media.loc[media['s3_key_index'].isna() & media['media_key_index'].isna()]) == 0)
+except AssertionError:
+    print(media.loc[media['s3_key_index'].isna() & media['media_key_index'].isna()])
+    raise
+if len(media.loc[media['s3_key_index'].isna()]) == 0:
+    sys.exit()
 
-sys.exit()
 
-new_thumb_values = []
-keys_to_remove = []
-failed_image_paths = []
-for index, row in tqdm(list(media.iterrows())):
-    ipath = os.path.join(root_dir, row['path_key'])
-    if not os.path.isfile(ipath):
-        print('Missing file:', ipath)
-        continue
+def generate_missing_thumbnail(row):
+    global my_s3_client
+    extension = row['media_key'].split('.')[-1]
+    with tempfile.TemporaryDirectory(prefix='thumb_') as tmp:
+        temp_path = os.path.join(tmp, 'image.' + extension)
+        my_s3_client.download_file(config.s3_bucket_name, row['media_key'], temp_path)
 
-    try:
-        metadata = process_image(ipath, run_hashing=False)
-    except Exception:
-        failed_image_paths.append(ipath)
-        continue
-    metadata['fpath_relative'] = os.path.relpath(ipath, root_dir)
+        max_size = (row['thumbnail_width'], row['thumbnail_height'])
+        with open(temp_path, 'rb') as f:
+            fdata = f.read()
+        image = PIL.Image.open(io.BytesIO(fdata))
+        thumbnail = PIL.ImageOps.exif_transpose(image).copy()
+        thumbnail.thumbnail(max_size)
+        thumbnail_path = os.path.join(tmp, 'thumbnail.jpg')
+        with open(thumbnail_path, 'wb') as f:
+            try:
+                thumbnail.save(f, format='JPEG')
+            except OSError:
+                thumbnail.convert('RGB').save(f, format='JPEG')
+        my_s3_client.upload_file(thumbnail_path, config.s3_bucket_name, row['thumbnail_key'])
 
-    if 'thumbnail' in metadata and 'thumbnail_path' in metadata['thumbnail'] and os.path.isfile(metadata['thumbnail']['thumbnail_path']):
-        fname, extension = os.path.splitext(metadata['fpath_relative'])
-        thumb_key = fname + '-%dw_%dh' % (metadata['thumbnail']['thumbnail_width'], metadata['thumbnail']['thumbnail_height']) + extension
-        try:
-            response = s3_client.upload_file(metadata['thumbnail']['thumbnail_path'], config.s3_bucket_name, thumb_key)
-            os.remove(metadata['thumbnail']['thumbnail_path'])
-            new_thumb_values.append((row['media_id'], thumb_key, int(metadata['thumbnail']['thumbnail_height']), int(metadata['thumbnail']['thumbnail_width']), int(metadata['thumbnail']['thumbnail_size'])))
-            if row['thumbnail_key'] != thumb_key:
-                keys_to_remove.append(row['thumbnail_key'])
-        except botocore.exceptions.ClientError as e:
-            print(e)
-            continue
 
-if len(failed_image_paths) > 0:
-    print('Failed image paths:', failed_image_paths)
-    with open('failed_image_paths.txt', 'w') as f:
-        for failed_image_path in failed_image_paths:
-            f.write(failed_image_path+'\n')
-
-pd.DataFrame(new_thumb_values, columns=['media_id', 'key', 'height', 'width', 'filesize']).to_csv('new_thumb.csv', index=False)
-
-df = pd.read_csv('new_thumb.csv')
-new_thumb_values = []
-for index, row in tqdm(list(df.iterrows())):
-    new_thumb_values.append(tuple(row.values))
-print('%d thumbnail rows to update' % len(new_thumb_values))
-
-with DatabaseConnector() as conn:
-    cursor = conn.cursor()
-    update_query = """UPDATE thumbnail AS t
-                    SET key = e.key,
-                    height = e.height,
-                    width = e.width,
-                    file_size = e.file_size
-                    FROM (VALUES %s) AS e(media_id, key, height, width, file_size)
-                    WHERE e.media_id = t.media_id;"""
-
-    psycopg2.extras.execute_values(
-        cursor, update_query, new_thumb_values, template=None, page_size=100,
+def init_worker():
+    os.nice(10)
+    my_session = boto3.session.Session(
+        aws_access_key_id=config.access_key,
+        aws_secret_access_key=config.secret_key,
     )
-    conn.commit()
+    global my_s3_client
+    my_s3_client = my_session.client(
+        "s3",
+        config=config.boto_config,
+        endpoint_url=config.s3_endpoint_url,
+    )
 
-print('%d thumbnail keys to remove' % len(keys_to_remove))
-for key in keys_to_remove:
-    s3_client.delete_object(Bucket=config.s3_bucket_name, Key=key)
+
+reporter = tqdm(total=len(media.loc[media['s3_key_index'].isna()]))
+
+
+def callback_helper(t):
+    reporter.update()
+
+
+pool = multiprocessing.Pool(processes=8, initializer=init_worker)
+for index, row in media.loc[media['s3_key_index'].isna()].iterrows():
+    pool.apply_async(generate_missing_thumbnail, args=(row,), callback=callback_helper)
+
+pool.close()
+pool.join()
+reporter.close()
