4b33da1ed80044629f9451196872622eaf76cbb3
Kyle Barlow <kyle.barlow@adimab.com>
2024-01-02 09:08:31 -0800

    Nice script and comment

--- run_pic_functions.sh
+++ run_pic_functions.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-
+# This trap business sems to not catch docker-compose down signals
 trap 'echo "Caught trap signal"' SIGHUP SIGINT SIGQUIT SIGTERM
 
 while :
 do
    echo "Starting Python script"
    date
-   python3 /webwork/picture_functions.py no_root_dir --num_processing_threads 1 --remove_after_upload --check_existing_hashes --check_existing_keys --sub_keys_to_scan kyle_phone_direct/2310_p6p malia_phone_direct/2310_p4a
+   nice python3 /webwork/picture_functions.py no_root_dir --num_processing_threads 1 --remove_after_upload --check_existing_hashes --check_existing_keys --sub_keys_to_scan kyle_phone_direct/2310_p6p malia_phone_direct/2310_p4a
    echo "Python script finished"
    date
    sleep 6000 &
