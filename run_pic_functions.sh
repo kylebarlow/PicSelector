dcd3138308654f1d427f417660db74e2fcbdb093
Kyle Barlow <kb@kylebarlow.com>
2024-01-10 15:08:19 -0800

    Change local script to run only once

--- run_pic_functions.sh
+++ run_pic_functions.sh
@@ -9,6 +9,7 @@ do
    date
    nice python3 /webwork/picture_functions.py no_root_dir --num_processing_threads 1 --remove_after_upload --check_existing_hashes --check_existing_keys --sub_keys_to_scan kyle_phone_direct/2310_p6p malia_phone_direct/2310_p4a
    echo "Python script finished"
+   break
    date
    # 20 hours = 72000 seconds
    sleep 72000 &
